54717f53fa5df0ea98759062c45642cf1b80b6f7
Natiq7855 <natabbasli@gmail.com>
2026-03-17 11:26:40 +0000

    SDF

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(0, 10):
-        if i == j:
+        if i >= j:
             continue
-        elif i > j:
-            continue
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
         else:
-            if str(i) + str(j) != "89":
-                print("{}{}".format(i, j), end = ", ")
-            else:
-                print("89")
-        
+            print("{}{}".format(i, j), end=", ")
