f1b43374bf8d46818a868e24aa329fdb1eab5f6d
Natiq7855 <natabbasli@gmail.com>
2026-03-17 11:49:58 +0000

    SDF

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
-        print(number % 10, end = "")
+        ln = number % 10
+        print(ln, end = "")
+        return ln
     else:
-        print(-number % 10, end = "")
+        ln = -number % 10
+        print(ln, end = "")
+        return ln
