5ebe22f1af71d14e2e433c2ecca1c7f56ac98b18
Natiq7855 <natabbasli@gmail.com>
2026-03-21 08:58:17 +0000

    SDF

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,8 +3,10 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if idx < 0:
         return None
+        return my_list
     elif idx >= length:
         return None
+        return my_list
     else:
         my_list[idx] = element
         return my_list
