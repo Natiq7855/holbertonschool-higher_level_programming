0561b42797f010dbbc67c6c45fc6d29321ebbf70
Natiq7855 <natabbasli@gmail.com>
2026-03-21 09:51:28 +0000

    sdf

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -18,6 +18,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) == 1 and len(tuple_b) == 1:
         tuple_sum = (tuple_a[0] + tuple_b[0], 0 + 0)
         return tuple_sum
-    elif len(tuple_a) == 0 and len(tuple_b) 0:
+    elif len(tuple_a) == 0 and len(tuple_b) == 0:
         tuple_sum = (0 + 0, 0 + 0)
         return tuple_sum
