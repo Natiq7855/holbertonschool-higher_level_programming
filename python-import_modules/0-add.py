3e23f898ccc60041074a88c26702010f1aed6322
Natiq7855 <natabbasli@gmail.com>
2026-03-21 07:36:43 +0000

    DSF

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    """Function that returns the addition of a and b"""
-    return (a + b)
+from add_0 import add
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
