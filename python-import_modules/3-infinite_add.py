131a8f8a228c00d8e321fae0809ed47bc8a8945c
Natiq7855 <natabbasli@gmail.com>
2026-03-21 08:16:46 +0000

    DSF

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     count = len(argv)
     result = 0
-    if count == 0:
-        print("0")
     for i in range(count):
         result = result + int(argv[i])
     print(result)
