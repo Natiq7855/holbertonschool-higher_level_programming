7657dcbd8d4c6fa23551085e089fd00c82b37afb
Natiq7855 <natabbasli@gmail.com>
2026-03-21 08:15:07 +0000

    DSF

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    count = len(argv)
+    if count == 0:
+        print("0")
+    for i in range(count):
+        result = result + int(argv[i])
+    print(result)
