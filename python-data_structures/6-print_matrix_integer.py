64a9999f305501b8f44729c7aa8a8806e87a5bb8
Natiq7855 <natabbasli@gmail.com>
2026-03-21 09:32:10 +0000

    DSF

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for j in matrix:
         for i in range(len(j)):
             if i != len(j) - 1:
-                print("{:d}".format(row[i]), end=" ")
+                print("{:d}".format(j[i]), end=" ")
             else:
-                print("{:d}".format(row[i]), end="")
+                print("{:d}".format(j[i]), end="")
         print()
