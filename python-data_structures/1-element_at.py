a4e49d5b62d3023e255c8258d0a6466ae2ee1e16
Natiq7855 <natabbasli@gmail.com>
2026-03-21 08:47:33 +0000

    DSF

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     length = len(my_list)
     if idx < 0:
         return None
-    elif idx >= length:
+    elif idx > length:
         return None
     else:
         print("Element at index {} is {}".format(idx, my_list[idx]))
