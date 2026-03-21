7f04325c0127b9da3e642a85836e4820f15401d7
Natiq7855 <natabbasli@gmail.com>
2026-03-21 08:41:19 +0000

    SDF

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print(my_list[i])
-print_list_integer(my_list = [1,2,3,4])
+    for i in my_list:
+        print("{:d}".format(i))
