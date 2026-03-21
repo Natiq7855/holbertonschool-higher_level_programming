4d455844099f90fc3f050ffce75ede74cf33299e
Natiq7855 <natabbasli@gmail.com>
2026-03-21 09:17:51 +0000

    SDF

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
+    for i in range(len(my_string) - 1):
         if my_string[i] == 'C' or my_string[i] == 'c':
             continue
         else:
