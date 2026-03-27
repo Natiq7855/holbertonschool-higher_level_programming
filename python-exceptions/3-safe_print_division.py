c37ee86c30a56924ead013c7a537f6c61eb2e99f
Natiq7855 <natabbasli@gmail.com>
2026-03-27 18:39:43 +0000

    SDF

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = 0
+        c = a / b
+    except (ZeroDivisionError, TypeError):
+        c = None
+    finally:
+        print("Inside result {}".format(c))
+    return result
