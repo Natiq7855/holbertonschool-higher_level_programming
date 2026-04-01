32151b82b1cdf0287e2734c4d8249577f452134d
Natiq7855 <natabbasli@gmail.com>
2026-04-01 17:12:59 +0400

    SDF

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,15 +7,15 @@ It is part of a series of exercises on Object-Oriented Programming.
 
 class Square:
     """An empty class Square that defines a square."""
-    def __init__(self, size):
+    def __init__(self, size = 0):
         """
         Initializes the square with a specific size.
 
         Args:
             size: The size of the square (no type/value verification).
         """
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
