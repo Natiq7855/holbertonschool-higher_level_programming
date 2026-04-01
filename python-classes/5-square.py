05b5a7a6a3694dc2f14cb14924d2a2ffb0c67b17
Natiq7855 <natabbasli@gmail.com>
2026-04-01 17:34:24 +0400

    SDF

---
+++ python-classes/5-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+It is part of a series of exercises on Object-Oriented Programming.
+"""
+
+
+class Square:
+    """An empty class Square that defines a square."""
+    def __init__(self, size=0):
+        """
+        Initializes the square with a specific size.
+
+        Args:
+            size: The size of the square (no type/value verification).
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the size of the square.
+
+        Returns:
+            The private __size attribute.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square with validation.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates the current square area.
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
+        for i in range(self.__size):
+            print("#" * self.__size)
