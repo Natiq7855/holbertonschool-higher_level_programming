54b91e77e77d30f7a28b6b08a59d4c4ac77098bf
Natiq7855 <natabbasli@gmail.com>
2026-04-01 18:26:38 +0400

    SDF

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -44,7 +44,10 @@ class Rectangle:
     def area(self):
         """function to find area"""
         return self.__height * self.__width
-    
+
     def perimeter(self):
         """function to find perimeter"""
-        return (self.__height + self.__width) * 2
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__height + self.__width) * 2
