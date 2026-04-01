f9fa5479a476d2a480fadab30222348ad156a5cd
Natiq7855 <natabbasli@gmail.com>
2026-04-01 17:58:13 +0400

    SDF

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -76,13 +76,13 @@ class Square:
         Raises:
             TypeError: If value is not an integer.
         """
-        if value[0] < 0 or value[1] < 0:
+        if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif not isinstance(value, tuple):
+        elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) != 2:
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
