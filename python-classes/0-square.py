4aa109665fa7d896c23376b5a1bf661dda9a0e8f
Natiq7855 <natabbasli@gmail.com>
2026-04-01 17:03:12 +0400

    SDF

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,7 +3,6 @@
 This module defines a Square class.
 It is part of a series of exercises on Object-Oriented Programming.
 """
-
 class Square:
     """An empty class Square that defines a square."""
     pass
