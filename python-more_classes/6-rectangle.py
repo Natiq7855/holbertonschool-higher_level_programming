91951dd1b384df2df2e4d710f8e9956c30ddf3c8
Natiq785 <natabbasli@gmail.com>
2026-04-01 23:48:08 +0400

    SDF

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -8,6 +8,7 @@ It is part of a series of exercises on Object-Oriented Programming.
 class Rectangle:
     """An empty class Rectangle that defines a square."""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
