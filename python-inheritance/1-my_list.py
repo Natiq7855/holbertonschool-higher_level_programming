7f315ba310eeb0e73b42272f35d24b1259549caa
Natiq7855 <natabbasli@gmail.com>
2026-04-07 16:51:42 +0400

    SDF

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a class MyList that inherits from list:"""
+
+
+class MyList(list):
+    """A class that inherits from list with an additional sorting display method."""
+
+    def print_sorted(self):
+    """Prints the list in ascending order without modifying the original."""
+ 
+        print(sorted(self))
