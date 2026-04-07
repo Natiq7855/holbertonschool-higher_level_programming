6279ce8c2010a47473ff42931a6c945ede2e2562
Natiq7855 <natabbasli@gmail.com>
2026-04-07 17:46:14 +0400

    SDF

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -40,13 +40,13 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
-    class Rectangle(BaseGeometry):
-        """rectangle class"""
+class Rectangle(BaseGeometry):
+    """rectangle class"""
 
-        def __init__(self, width, height):
-            """set width and height"""
+    def __init__(self, width, height):
+        """set width and height"""
 
-            self.integer_validator("width", width)
-            self.__width = width
-            self.integer_validator("height", height)
-            self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
