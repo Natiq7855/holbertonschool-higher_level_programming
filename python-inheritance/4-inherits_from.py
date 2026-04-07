a64a0ff3329738be61a165a12745efafa72a2e89
Natiq7855 <natabbasli@gmail.com>
2026-04-07 17:13:31 +0400

    SDF

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """fuction of it"""
 
-    return isinstance(obj, a_class) and obj is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
