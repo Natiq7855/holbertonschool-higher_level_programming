40008ba298b08944fc0627d52ff19658eb38f432
Natiq7855 <natabbasli@gmail.com>
2026-04-14 14:12:33 +0400

    SDF

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""sda asd"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """func of"""
+
+    with open(filename, mode="w", encoding="UTF-8") as f:
+        json.dumps(my_obj, f)
