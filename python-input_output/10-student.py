90494d10c26177d1b6f560479b4c3fe240aa57d4
Natiq7855 <natabbasli@gmail.com>
2026-04-14 14:30:47 +0400

    SDF

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class func"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """initialize func"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json func"""
+        if isinstance(attrs, list) and all(isinstance(item, str) for item in attrs):
+            res = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    res[key] = self.__dict__[key]
+            return res
+        return self.__dict__
