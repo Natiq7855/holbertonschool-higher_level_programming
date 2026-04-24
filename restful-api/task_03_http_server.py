50e3621096066e245f12b8f9fe10c4c3c171f8a0
Natiq7855 <natabbasli@gmail.com>
2026-04-24 16:51:53 +0400

    SDF

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import http.server
+import json
+"""asd dsgadf"""
+
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+    """asda  wee0"""
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
