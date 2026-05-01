ee31482314ae83762c76cd6ed80a524e09870f65
Natiq7855 <natabbasli@gmail.com>
2026-05-01 16:26:12 +0400

    SDF

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,6 @@
--- Lists all records of second_table ordered by score (top first)
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- Lists all records with a score >= 10 in the table second_table
+-- Results display score and name, ordered by score descending
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
