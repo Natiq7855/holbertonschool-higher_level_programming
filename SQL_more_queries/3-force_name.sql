0b9d8308afe68ef0ff616bf563dbbd871a9c5bf8
Natiq785 <natabbasli@gmail.com>
2026-05-02 13:17:48 +0400

    SDF

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,2 @@
 --force_name table creation
-CREATE TABLE IF NOT EXISTS force_name(
-    id INT,
-    name VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
