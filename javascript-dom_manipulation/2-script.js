d9697e02ee93c31185a2c58a0caf9c9f8dcec0dc
Natiq7855 <natabbasli@gmail.com>
2026-05-16 16:11:23 +0400

    SDF

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,8 +1,6 @@
-// Select the header element and the tag with the ID 'red_header'
 const header = document.querySelector('header');
 const redHeaderButton = document.querySelector('#red_header');
 
-// Ensure both elements exist before adding the event listener
 if (redHeaderButton && header) {
     redHeaderButton.addEventListener('click', () => {
         header.classList.add('red');
