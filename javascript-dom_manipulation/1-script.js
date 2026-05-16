b64334f2f85fbebfc38bce357f10feba4fef51e8
Natiq7855 <natabbasli@gmail.com>
2026-05-16 16:06:26 +0400

    SDF

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,11 +1,9 @@
-#!/usr/bin/node
-
-const header = document.querySelector("h1");
+const header = document.querySelector("header");
 const button = document.querySelector("red_header");
 
 if (button && header){
     button.addEventListener('click', () => {
-        header.style.color = #FF0000;
+        header.style.color = '#FF0000';
     })
 } else{
     console.warn("something worng")
